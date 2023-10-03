3e0e61edb54c4219b7bc68237d8e65ecb12b503e
kah-g <karinnegv@hotmail.com>
2023-10-03 19:54:49 +0100

    terminando exercicios da aula 2

--- Programacao_I_ipv/aula_2.c
+++ Programacao_I_ipv/aula_2.c
@@ -9,24 +9,24 @@ void main () {
     switch (exer)
     {
     case 1:
-        //1.crie um programa que mostre no ecra o texto hello world
+        printf("1.Crie um programa que mostre no ecra o texto hello world.\n");
         printf("Hello world\n");
         break;
 
     case 2:
-        //2.declare uma variavel e atribua o valor 10. Imprima o seu valor
+        printf("2.Declare uma variavel e atribua o valor 10. Imprima o seu valor.\n");
         int num = 10;
         printf("%d\n", num);
         break;
 
     case 3:
-        //3.declare uma variavel e atribua o valor 3,14159. Imprima o seu valor com duas casas decimais
+        printf("3.Declare uma variavel e atribua o valor 3,14159. Imprima o seu valor com duas casas decimais.\n");
         float num2 = 3.14159;
         printf("%.2f\n", num2);
         break;
 
     case 4:
-        //4.solicite o nome do usuario como entrada e imprime uma mensagem de boas vindas que inclui o nome do usuario
+        printf("4.Solicite o nome do usuario como entrada e imprime uma mensagem de boas vindas que inclui o nome do usuario.\n");
         printf("Entre com o seu nome:\n");
         char nome[10];
         scanf("%s", &nome);
@@ -34,7 +34,7 @@ void main () {
         break;
 
     case 5:
-        //5.calcule a area de um retangulo. Solicite ao usuario o comprimento e a largura como entrada
+        printf("5.Calcule a area de um retangulo. Solicite ao usuario o comprimento e a largura como entrada.\n");
         float comp, larg, area;
         printf("Digite o comprimento do retangulo:\n");
         scanf("%f", &comp);
@@ -45,7 +45,7 @@ void main () {
         break;
 
     case 6:
-        //6.converta graus celsius para fahrenheit. Receba a temperatura em celsius como entrada
+        printf("6.Converta graus celsius para fahrenheit. Receba a temperatura em celsius como entrada.\n");
         float cel, fah;
         printf("Digite a temperatura em celsius:\n");
         scanf("%f", &cel);
@@ -54,24 +54,34 @@ void main () {
         break;
 
     case 7:
-        //7.verifique se um numero e par ou impar
+        printf("7.Verifique se um numero e par ou impar.\n");
         int num3;
-        printf("Entre com um numero inteiro\n");
+        printf("Entre com um numero inteiro:\n");
         scanf("%d", &num3);
         float result = num3%2;
         if (result == 0) {
-            printf("o numero e par\n");
+            printf("o numero e par.\n");
         } else {
-            printf("o numero e impar\n");
+            printf("o numero e impar.\n");
         }
         break;
 
     case 8:
-        //8.trocar o valor de duas variavies
+        printf("8.Trocar o valor de duas variavies, sem usar uma variavel temporaria.\n");
+        int num8A, num8B;
+        printf("Entre com o primeiro numero:\n");
+        scanf("%d", &num8A);
+        printf("Entre com o segundo numero:\n");
+        scanf("%d", &num8B);
+        printf("Antes da troca - Numero A: %d, numero B: %d.\n", num8A, num8B);
+        num8A = num8A + num8B;
+        num8B = num8A - num8B;
+        num8A = num8A - num8B;
+        printf("Depois da troca - Numero A: %d, numero B: %d.\n", num8A, num8B);
         break;
 
     case 9:
-        //9.encontre o maior de tres numeros --pode ser mais curto
+        printf("9.Encontre o maior de tres numeros.\n");
         int numA, numB, numC;
         printf("Entre com o primeiro numero:\n");
         scanf("%d", &numA);
@@ -79,13 +89,9 @@ void main () {
         scanf("%d", &numB);
         printf("Entre com o terceiro numero:\n");
         scanf("%d", &numC);
-        if (numA >= numB) {
-            if (numA >= numC) {
-                printf("%d e o maior numero\n", numA);
-            } else {
-                printf("%d e o maior numero\n", numC);
-            }
-        } else if (numB >= numC) {
+        if (numA >= numB && numA >= numC) {
+            printf("%d e o maior numero\n", numA);
+        } else if (numB >= numA && numB >= numC) {
             printf("%d e o maior numero\n", numB);
         } else {
             printf("%d e o maior numero\n", numC);
@@ -93,7 +99,7 @@ void main () {
         break;
 
     case 10:
-       printf("Exercicio 10 - Verifique se um ano e bissexto ou nao.\n");
+       printf("10.Verifique se um ano e bissexto ou nao.\n");
         int ano;
         printf("Entre um ano valido:\n");
         scanf("%d", &ano);
@@ -105,7 +111,7 @@ void main () {
         break;
 
     case 11:
-        printf("Exercicio 11 - Receba a idade do usuario como entrada e determina se e criança (0-12), adolescente (13-17), adulto (18-64) ou idoso (65+).\n");
+        printf("11.Receba a idade do usuario como entrada e determina se e criança (0-12), adolescente (13-17), adulto (18-64) ou idoso (65+).\n");
         int idade;
         printf("Digite a idade:\n");
         scanf("%d", &idade);
@@ -121,7 +127,7 @@ void main () {
         break;
 
     case 12:
-        printf("Exercicio 12 - Determine se um numero e positivo, negativo ou zero.\n");
+        printf("12.Determine se um numero e positivo, negativo ou zero.\n");
         int num4;
         printf("Entre com um numero inteiro:\n");
         scanf("%d", &num4);
@@ -137,7 +143,7 @@ void main () {
         break;
 
     case 13:
-        printf("Exercicio 13 - Verifique se um numero e divisivel por 5 ou 7.\n");
+        printf("13.Verifique se um numero e divisivel por 5 ou 7.\n");
         int num5;
         printf("Entre com um numero inteiro:\n");
         scanf("%d", &num5);
@@ -149,7 +155,7 @@ void main () {
         break;
 
     case 14:
-        printf("Exercicio 13 - Verifique se um numero e multiplo de 3 e 5 ao mesmo tempo.\n");
+        printf("14.Verifique se um numero e multiplo de 3 e 5 ao mesmo tempo.\n");
         int num6;
         printf("Entre com um numero inteiro:\n");
         scanf("%d", &num6);
@@ -166,6 +172,40 @@ void main () {
         printf("Entre com um numero intero:\n");
         scanf("%d", &num7);
         printf("%d\n", num7 * num7);
+        break;
+
+    case 16:
+        printf("16 - Calcule o valor absoluto de um numero.\n");
+        int num9;
+        printf("Entre com um numero intero:\n");
+        scanf("%d", &num9);
+        if (num9 < 0) {
+            printf("%d\n", num9 * -1);
+        }
+        else {
+            printf("%d\n", num9);
+        }
+        break;
+
+    case 17:
+        printf("17.Calcule a média de três notas inseridas pelo utilizador e determine se o aluno foi aprovado ou reprovado. A média mínima para aprovação é 9.5.\n");
+        float notaA, notaB, notaC, media;
+        printf("Entre com a primeira nota:\n");
+        scanf("%f", &notaA);
+        printf("Entre com a segunda nota:\n");
+        scanf("%f", &notaB);
+        printf("Entre com a terceira nota:\n");
+        scanf("%f", &notaC);
+        media = (notaA + notaB + notaC) / 3;
+        if (media >= 9.5) {
+            printf("Nota: %.1f - Aprovado!\n", media);
+        } else {
+            printf("Nota: %.1f - Reprovado!\n", media);
+        }
+        break;
+
+    case 18:
+        printf("18.Converta um número inteiro em seu equivalente em binário.\n");
         break;
     
     default:
